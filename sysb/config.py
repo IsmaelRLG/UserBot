97b3eaf6c2572b167380e0d0ce6a0422f36b3f1a
IsmaelRLG <IsmaelRLGV@Gmail.com>
2015-08-02 09:48:48 -0430

    Error menor solucionado

--- sysb/config.py
+++ sysb/config.py
@@ -9,9 +9,8 @@ class config(database.ownbot):
     def __init__(self):
         # Creamos la linda tabla que almacenara toda la configuracion <3
         self.create('core', 'id text key not null, pick text not null', True)
-        try:
-            self.obtconfig('VERSION')
-        except IndexError:
+
+        if self.obtconfig('VERSION') is None:
             self.addconfig('VERSION', ('UserBot', 0, 7, 54))
 
     def addconfig(self, name, _object):
@@ -25,8 +24,11 @@ class config(database.ownbot):
         self.delete('core', nam)
 
     def obtconfig(self, nam):
-        return cPickle.loads(
+        try:
+            return cPickle.loads(
             self.select('core', 'pick', 'id="%s"' % nam)[0][0].encode('utf-8'))
+        except IndexError:
+            pass
 
     def upconfig(self, name, _object):
         self.update('core',
