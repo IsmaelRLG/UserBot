f202bc5fddf74801ec056d4bec85cf4bda96af7f
Ismael <ismaelrlgv@gmail.com>
2015-12-06 16:16:58 -0430

    UserBot module: help [agregado]

---
+++ mods/help.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+from irc.request import whois
+from sysb.config import core
+from sysb import commands
+from sysb import i18n
+from irc.connection import servers as base
+
+import textwrap
+
+locale = i18n.turn(
+    'es',
+    core.obtconfig('package_translate'),
+    'help')
+_ = locale.turn_tr_str
+lang = core.obtconfig('lang')
+cmls = {}
+
+
+def make_cmls():
+    for module, dic in commands.commands.modules.items():
+        for handlers in dic['handlers']:
+            help = handlers['help']['sintax'].lower().split()
+            help.reverse()
+
+            first = help.pop()
+
+            try:
+                second = help.pop()
+            except IndexError():
+                second = None
+            if not first in cmls:
+                cmls[first] = {}
+
+            if not '<' in second and\
+               not '>' in second and\
+               not '?' in second and\
+               not '|' in second:
+
+                if not 'sub' in cmls[first]:
+                    cmls[first].update({'sub': {}})
+                cmls[first]['sub'][second] = handlers['help']
+            else:
+                cmls[first].update(handlers['help'])
+
+
+def search(ls):
+    ls.reverse()
+    first = ls.pop()
+    if first in cmls:
+        if len(ls) == 0:
+            if 'sintax' in cmls[first]:
+                return cmls[first]
+        elif 'sub' in cmls[first] and ls[len(ls) - 1] in cmls[first]['sub']:
+            return cmls[first]['sub'][ls.pop()]
+
+    return {}
+
+
+@commands.addHandler('help', '(help|man|ayuda)( (?P<command>.*))?', {
+    'sintax': 'help <command>?',
+    'example': 'help help',
+    'desc': _('muestra la ayuda de algun comando', lang)},
+    anyuser=True)
+def help(irc, result, group, other):
+    rpl = whois(irc, group('nick'))
+    lang = other['global_lang']
+    user = None
+    command = result('command')
+    target = other['target']
+    if rpl['is logged'] and base[irc.base.name][1][rpl['is logged']]:
+        user = base[irc.base.name][1][rpl['is logged']]
+
+    if user:
+        lang = user['lang']
+
+    vrn = core.obtconfig('VERSION')
+    vrn = (vrn[0] + ' ' + '.'.join(str(num) for num in vrn[1:]))
+    irc.notice(target, vrn + ' - ' + _('codigo fuente: ', lang) + 'https://goo.gl/vVSG8i')
+
+    if command:
+        res = search(command.lower().split())
+        if res:
+            irc.notice(target, _('ayuda para: ', lang) + command)
+            if isinstance(res['desc'], tuple) or isinstance(res['desc'], list):
+                irc.notice(target, _('descripcion: ', lang))
+                for line in res['desc']:
+                    irc.notice(target, '        ' + line)
+            else:
+                irc.notice(target, _('descripcion: ', lang) + res['desc'])
+
+            irc.notice(target, _('sintaxis: ', lang) + res['sintax'])
+            irc.notice(target, _('ejemplo: ', lang) + res['example'])
+        else:
+            irc.notice(target, _('el comando no existe', lang))
+    else:
+        post = {}
+        first = []
+
+        for dic in cmls.items():
+            if 'sub' in dic[1]:
+                if not dic[0] in post:
+                    post[dic[0]] = []
+
+                for sub in dic[1]['sub']:
+                    post[dic[0]].append(sub)
+            else:
+                first.append(dic[0])
+
+        irc.notice(target, _('comandos disponibles: ', lang))
+        msg = textwrap.wrap(', '.join(first), 400)
+        for o in msg:
+            irc.notice(target, o)
+
+        for pr, sub in post.items():
+            if pr == 'oper':
+                if user and user['status'] in ('noob', 'local', 'global'):
+                    for o in textwrap.wrap(', '.join(sub), 400):
+                        irc.notice(target, '%s: %s' % (pr, o))
+                else:
+                    irc.notice(target, 'oper: id')
+            else:
+                for o in textwrap.wrap(', '.join(sub), 400):
+                    irc.notice(target, '%s: %s' % (pr, o))
+
+        irc.notice(target, _('envie: help <comando> <subcomando>?', lang))
+
+make_cmls()
